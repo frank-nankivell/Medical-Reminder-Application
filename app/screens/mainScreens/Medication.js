63859183f9537e07109048833afe43ad5ad5411a
franknankivell <fn17aak@herts.ac.uk>
2019-04-19 11:11:07 +0100

    updates to form

--- app/screens/mainScreens/Medication.js
+++ app/screens/mainScreens/Medication.js
@@ -6,7 +6,9 @@ import lightWhite from '../../constants/colors';
 import List from '../../components/List';
 import ButtonNew from '../../components/Button';
 import InputMedication from '../../components/InputMedication';
+import DatePicker from 'react-native-datepicker'
 import uuid from 'uuid/v1';
+import green1 from '../../constants/colors';
 import InputDate from '../../components/InputDate'
 import { Calendar, 
         CalendarList, 
@@ -20,6 +22,7 @@ import {
     Dimensions,
     Text,
     View,
+    TextInput,
     Picker,
     TouchableOpacity,
     ActivityIndicator,
@@ -27,6 +30,7 @@ import {
     ImageBackground,
     StyleSheet } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
+
 
 
 const headerTitle = 'Medication Reminder';
@@ -41,45 +45,52 @@ class Medication extends Component {
     this.state = {
         inputValue: '',
         inputDosage: '',
-        year: '',
-        month: '',
-        day: '',
-        date: '',
-       chosenDate: new Date(),
-       loadingItems: false,
-       isCompleted: false
+        inputPerDay: '',
+        inputInterval:'',
+        inputNotes: '',
+        inputEndDate: '',
+        Created: new Date(),
+        loadingItems: false,
+        isCompleted: false
 
     };
-        this.setDate = this.setDate.bind(this);
-        componentDidMount = () => {
-          this.loadingItems();
-        };
-    };
+};
+  
+    // Method for updating Value
     newInputValue = value => {
         this.setState({
           inputValue: value,
         });
       };
-
+      // Method for updating Dosage
       newInputDosage = value => {
         this.setState({
           inputDosage: value
         });
       };
-
-      newMonth = value => {
+      // Method for updating per day 
+      newInputPerDay  =  value => {
         this.setState({
-          month: value
+          inputPerDay: value
         });
       };
-      newDay = value => {
+      // Method for updating Interval 
+      newInputInterval  =  value => {
         this.setState({
-          day: value
+          inputInterval: value
         });
       };
-
-      setDate = newDate => {
-        this.setState({chosenDate: newDate});
+    // Method for updating Notes
+      newInputNotes  =  value => {
+        this.setState({
+          inputNotes: value
+        });
+      };
+      // Method for updating End Date
+      newInputEndDate  =  value => {
+        this.setState({
+          inputEndDate: value
+        });
       };
 
       loadingItems = async () => {
@@ -183,65 +194,115 @@ class Medication extends Component {
       };
 
     render() {
-        const { inputValue, inputDosage, loadingItems, day, month } = this.state; 
+        const { inputValue, inputDosage, inputPerDay, inputInterval, inputNotes, inputEndDate  } = this.state; 
         return (
             <ImageBackground source={bgImage} style={styles.backgroundContainer}>
+            
             <View style={styles.centered}>
               <Header title={headerTitle} />
             </View>
-            <View>
+
+            <View style={styles.centered} color='white'>
               <Text> Below enter your medication dosage and end date to create a reminder </Text>
             </View>
+
             <View style={styles.inputContainer}>
-              <InputText 
-                inputValue={inputValue} 
-                onChangeText={this.newInputValue} />
+                <InputText // input Value
+                    style={styles.input}
+                    inputValue={inputValue} 
+                    onChangeText={this.newInputValue} />
 
-              <Picker 
-                selectedValue={this.state.inputDosageNumber}
-                onValueChange={(itemValue, itemIndex) => this.setState({inputDosageNumber: itemValue})}>     
-    
-                <Picker.Item label="1" value ='1' />
-                <Picker.Item label="2" value ='2' />
-                <Picker.Item label="3" value ='3' />
-                <Picker.Item label="4" value ='4' />
-                <Picker.Item label="5" value ='5' />
-                <Picker.Item label="6" value ='6' />
-                <Picker.Item label="7" value ='7' />
-                <Picker.Item label="8" value ='8' />
-                <Picker.Item label="9" value ='9' />
-                <Picker.Item label="10" value ='10' />
-                <Picker.Item label="11" value ='11' />
-                <Picker.Item label="12" value ='12' />
-                <Picker.Item label="13" value ='13' />
+                  <TextInput // input dosage 
+                    style={styles.input}
+                    placeholder='Enter Dosage'
+                    keyboardType='numeric'
+                    onChangeText={(text)=> this.onChanged(text)}
+                    value={this.state.inputDosage}
+                    maxLength={10}  //setting limit of input
+                  />
 
-              </Picker>
-              <Text> times per week</Text>
-              <Picker
-                label = 'days per week'
-                selectedValue={this.state.inputDosageType}
-                onValueChange={(itemValue, itemIndex) => this.setState({inputDosage: itemValue})}>     
-    
-                <Picker.Item label="per week" value ='week' />
-                <Picker.Item label="per day" value ='day' />
-
-              </Picker>
-              <Text> date the medication will finish</Text>
-              <DatePickerIOS
-                style={styles.dateStyle}
-                date={this.state.chosenDate}
-                onDateChange={this.setDate}
-                />
-
+                  <TextInput // input Per day 
+                    style={styles.input}
+                    keyboardType='numeric'
+                    placeholder='Enter Amount per day'
+                    onChangeText={(text)=> this.onChanged(text)}
+                    value={this.state.inputPerDay}
+                    maxLength={10}  //setting limit of input
+                  />
+                  <DatePicker // input interval
+                    style={styles.inputDate}
+                    date={this.state.inputInterval}
+                    mode="time"
+                    placeholder="Select Interval Time"
+                    confirmBtnText="Confirm"
+                    cancelBtnText="Cancel"
+                    customStyles={{
+                      dateInput: {
+                        marginHorizontal: 25,
+                        width: WIDTH -55,
+                        height:40,
+                        borderRadius: 45,
+                        backgroundColor: '#6EC3CF',
+                        justifyContent: 'center',
+                        marginTop: 10,
+                        marginBottom: 25,
+                        opacity: 0.75
+                      }
+                      // ... You can check the source to find the other keys.
+                    }}
+                    onDateChange={(time) => {this.setState({inputInterval: time})}}
+                    />
+                  <TextInput // input times per day  
+                    style={styles.input}
+                    placeholder='Enter Times per Day'
+                    keyboardType='numeric'
+                    onChangeText={(text)=> this.onChanged(text)}
+                    value={this.state.inputInterval}
+                    maxLength={10}  //setting limit of input
+                  />
+               
+                <DatePicker // input end date 
+                    style={styles.inputDate}
+                    date={this.state.inputEndDate}
+                    mode="date"
+                    placeholder="select end date"
+                    format="YYYY-MM-DD"
+                    confirmBtnText="Confirm"
+                    cancelBtnText="Cancel"
+                    customStyles={{
+                      dateInput: {
+                        marginHorizontal: 25,
+                        width: WIDTH -55,
+                        height:40,
+                        borderRadius: 45,
+                        backgroundColor: '#6EC3CF',
+                        justifyContent: 'center',
+                        marginTop: 10,
+                        marginBottom: 25,
+                        opacity: 0.75
+                      }
+                      // ... You can check the source to find the other keys.
+                    }}
+                    onDateChange={(date) => {this.setState({inputEndDate: date})}}
+                  />
+                  <TextInput // input Notes
+                    style={styles.input}
+                    placeholder='Enter Notes'
+                    onChangeText={(text)=> this.onChanged(text)}
+                    value={this.state.inputNotes}
+                    maxLength={10}  //setting limit of input
+                  />
+                  </View>
                 <TouchableOpacity style={styles.btnSubmit}>
+                
                 <Button 
                   title="Submit" 
                   color="white" 
                   onPress={this.onDoneAddItem}>
                 </Button>
                 </TouchableOpacity>
-            </View>
-
+  
+              
             </ImageBackground>
   
         );
@@ -262,10 +323,6 @@ const styles = StyleSheet.create ({
         },
         container: {
             flex: 1
-        },
-        inputContainer: {
-            marginTop: 40,
-            paddingLeft: 15
         },
         scrollableList: {
           marginTop: 15
@@ -291,28 +348,38 @@ const styles = StyleSheet.create ({
             marginRight: 40
           },
           inputContainer: {
-            flex: 1,
-            marginTop: 70,
-            paddingLeft: 15,
-            justifyContent: 'space-between',
-          },
-          dateStyle: {
+            flex: 1, 
             width: WIDTH -55,
-            height: 100,
-            borderRadius: 45,
-            fontSize: 30,
-            backgroundColor: 'rgba(255,255,255,0.7)',
-            justifyContent: 'center',
-            marginTop: 30,
-            opacity: 1
+            borderRadius: 55,
+            alignItems: 'center',
+            backgroundColor:'white',
           },
+          input: {
+            paddingLeft: 45,
+            flex: 0.3,
+            fontSize: 25,
+            width: WIDTH -55,
+            borderRadius: 55,
+            alignItems: 'center',
+            borderColor: 'rgba(0,0,0,0.5)',
+            backgroundColor:  'white',
+            color: 'white'
+          },
+          inputDate: {
+            flex: 0.3,
+            width: WIDTH -55,
+            borderRadius: 55,
+          },
+
           btnSubmit: {
+            marginHorizontal: 25,
             width: WIDTH -55,
-            height: 45,
+            height:40,
             borderRadius: 45,
-            backgroundColor: '#4EEEFF',
+            backgroundColor: 'rgba(0,0,0,0.5)',
             justifyContent: 'center',
-            marginTop: 30,
+            marginTop: 10,
+            marginBottom: 25,
             opacity: 0.75
           },
 });
